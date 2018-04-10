b9907cf42bb9ec25c60a21eeb57d56ac43d0dd6f
Mekyi <no-reply>
2018-04-10 10:09:39 +0300

    added keyboard support

--- client.py
+++ client.py
@@ -1,9 +1,10 @@
 import pygame
 from socket import *
+from pygame.locals import *
 
 ctrl_cmd = ['forward', 'backward', 'left', 'right', 'stop', 'home']
 
-HOST = '192.168.1.6'
+HOST = '192.168.1.5'  # 10.0.0.184
 PORT = 21567
 BUFSIZ = 1024
 ADDR = (HOST, PORT)
@@ -102,6 +103,22 @@ while done == False:
         if event.type == pygame.JOYBUTTONUP:
             print("Joystick button released.")
 
+        # Keyboard input
+        elif event.type == KEYDOWN:
+            if event.key in (K_UP, K_w):
+                forward()
+            elif event.key in (K_DOWN, K_s):
+                backward()
+            elif event.key in (K_LEFT, K_a):
+                left()
+            elif event.key in (K_RIGHT, K_d):
+                right()
+        elif event.type == KEYUP:
+            if event.key in (K_LEFT, K_a) or event.key in (K_RIGHT, K_d):
+                home()
+            elif event.key in (K_UP, K_w) or event.key in (K_DOWN, K_s):
+                stop()
+
     # DRAWING STEP
     # First, clear the screen to white.  Don't put other drawing commands
     # above this, or they will be erased with this command.
@@ -122,29 +139,33 @@ while done == False:
         textPrint.indent()
 
         # Get the name from the OS for the controller/joystick
+
         name = joystick.get_name()
         textPrint.print(screen, "Joystick name: {}".format(name))
 
         # Usually axis run in pairs, up/down for one, and left/right for
         # the other.
-        axes = joystick.get_numaxes()
-        textPrint.print(screen, "Number of axes: {}".format(axes))
-        textPrint.indent()
-        for i in range(axes):
-            axis = joystick.get_axis(i)
-            textPrint.print(screen, "Axis {} value: {:>6.3f}".format(i, axis))
-        textPrint.unindent()
 
-        buttons = joystick.get_numbuttons()
-        textPrint.print(screen, "Number of buttons: {}".format(buttons))
-        textPrint.indent()
+        #axes = joystick.get_numaxes()
+        #textPrint.print(screen, "Number of axes: {}".format(axes))
+        #textPrint.indent()
+        #for i in range(axes):
+        #    axis = joystick.get_axis(i)
+        #    textPrint.print(screen, "Axis {} value: {:>6.3f}".format(i, axis))
+        #textPrint.unindent()
 
-        for i in range(buttons):
-            button = joystick.get_button(i)
-            textPrint.print(screen, "Button {:>2} value: {}".format(i,button))
-        textPrint.unindent()
+        #buttons = joystick.get_numbuttons()
+        #textPrint.print(screen, "Number of buttons: {}".format(buttons))
+        #textPrint.indent()
+
+        #for i in range(buttons):
+        #    button = joystick.get_button(i)
+        #    textPrint.print(screen, "Button {:>2} value: {}".format(i,button))
+        #textPrint.unindent()
+
         # Hat switch.  All or nothing for direction, not like joysticks.
         # Value comes back in an array.
+
         hats = joystick.get_numhats()
         textPrint.print(screen, "Number of hats: {}".format(hats))
         textPrint.indent()
@@ -152,12 +173,16 @@ while done == False:
         for i in range(hats):
             hat = str(joystick.get_hat(i))
             textPrint.print(screen, "Hat {} value: {}".format(i, hat))
-            if (hat == '(0, 1)'):
-                forward()
-            elif (hat == '(0, -1'):
-                backward()
-            elif (hat == '(0, 0)'):
-                stop()
+            #if (hat == '(0, 1)'):
+            #    forward()
+            #elif (hat == '(0, -1)'):
+            #    backward()
+            #elif (hat == '(0, 0)'):
+            #    stop()
+            #elif (hat == '(-1, 0)'):
+            #    left()
+            #elif (hat == '(1, 0)'):
+            #    right()
         textPrint.unindent()
 
         textPrint.unindent()
